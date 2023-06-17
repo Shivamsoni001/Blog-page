4c2f93adfe24c1d78e604f19f4a836d1b830d16a
Shivamsoni001 <shivam25ic053@satiengg.in>
2023-06-17 19:53:25 +0530

    Blog-page
    day 15

---
+++ new/public/js/firebase.js
@@ -0,0 +1,14 @@
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyAzmB_ytb_5q0poUjOPnt1_ey3oMxOkiVw",
+    authDomain: "blogging-website-738f7.firebaseapp.com",
+    projectId: "blogging-website-738f7",
+    storageBucket: "blogging-website-738f7.appspot.com",
+    messagingSenderId: "170377293065",
+    appId: "1:170377293065:web:14dc1ddee8e6ff96c1f7d8"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  let ab = firebase.firestore();
